9a136f87f01d1427784d6dd859cfb80b63285fcb
Jaejoon Han <jj2726@gmail.com>
2024-11-01 15:00:36 +0900

    detect file changed

--- Controllers/WebhookController.cs
+++ Controllers/WebhookController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.Json;
 
 namespace dotnet_github_webhook.Controllers;
 
@@ -27,6 +29,61 @@ public class WebhookController(IConfiguration configuration) : ControllerBase
         var eventType = Request.Headers["X-GitHub-Event"].ToString();
         if (eventType == "push")
         {
+            string? compareUrl = null;
+
+            using (var document = JsonDocument.Parse(payload))
+            {
+                var root = document.RootElement;
+                compareUrl = root.GetProperty("compare").GetString();
+            }
+
+            if (compareUrl is null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var apiUrl = compareUrl.Replace("https://github.com/", "https://api.github.com/repos/");
+
+            using var httpClient = new HttpClient();
+            var token = configuration["Github:Pat"] ?? throw new InvalidOperationException();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Token", token);
+            httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("MyApp", "1.0"));
+
+            var response = await httpClient.GetAsync(apiUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+
+                using var document = JsonDocument.Parse(responseContent);
+                var root = document.RootElement;
+
+                if (root.TryGetProperty("files", out JsonElement filesElement) && filesElement.ValueKind == JsonValueKind.Array)
+                {
+                    foreach (var fileElement in filesElement.EnumerateArray())
+                    {
+                        var filename = fileElement.GetProperty("filename").GetString();
+                        var status = fileElement.GetProperty("status").GetString();
+
+                        string? patch = null;
+                        if (fileElement.TryGetProperty("patch", out JsonElement patchElement))
+                        {
+                            patch = patchElement.GetString();
+                        }
+
+                        Console.WriteLine($"""
+                        File Name: {filename}"
+                        Status: {status}
+                        {(!string.IsNullOrEmpty(patch) ? $"Content:\n{patch}" : "")}
+                        --------------------------------------------------------------
+                        """);
+                    }
+                }
+            }
+            else
+            {
+                return NotFound();
+            }
         }
 
         return Ok();
